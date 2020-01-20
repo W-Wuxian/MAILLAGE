19f9231b928108b47738509313a185b53b7d3457
cmalvinerie <43368177+cmalvinerie@users.noreply.github.com>
2020-01-20 11:43:38 +0100

    fonction pour savoir si point dans triangle
    Déjà écrit avec les variables du langage de base 
    A intégrer au prog

---
+++ barycentre.f90
@@ -0,0 +1,16 @@
+
+
+!Programme qui retourne le signe des coordonnées barycentriques et si lepoint est dans le triangle ou pas 
+
+function produit_vecteur(
+  
+program baryc
+
+  implicit none
+
+  real :: xa,xb,xc,xd,&
+       & ya,yb,yc,yp
+
+  
+
+end program baryc
